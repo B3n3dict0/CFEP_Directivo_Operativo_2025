0cd912fb2a5901b0e1345e735517a4504b9194fd
benedictodzibdzib@gmail.com <benedictodzibdzib@gmail.com>
2025-09-26 11:04:13 -0600

    copia seguridad faltante es directivo reglas

--- operativo/views.py
+++ operativo/views.py
@@ -83,38 +83,7 @@ def historial_acuerdos(request):
 
 
 #logica de crear acuerdo 
-def crear_acuerdo_operativo(request):
-    integrantes = Integrante.objects.all().order_by('area__nombre', 'nombre_completo')
-    unidades = list(range(1, 10))  # Para el dropdown de unidades
 
-    if request.method == "POST":
-        filas = request.POST.getlist('numerador')
-        unidades_post = request.POST.getlist('unidad')
-        acuerdos = request.POST.getlist('acuerdo')
-        unidad_paradas = request.POST.getlist('unidad_parada')
-        fechas_limite = request.POST.getlist('fecha_limite')
-        pendientes = request.POST.getlist('pendiente')
-        responsables = request.POST.getlist('responsable')
-        avances = request.POST.getlist('porcentaje_avance')
-
-        for i in range(len(filas)):
-            AcuerdoOperativo.objects.create(
-                numerador=int(filas[i]),
-                unidad=int(unidades_post[i]),
-                acuerdo=acuerdos[i],
-                unidad_parada=(unidad_paradas[i] == 'on') if i < len(unidad_paradas) else False,
-                fecha_limite=fechas_limite[i],
-                pendiente=(pendientes[i] == 'on') if i < len(pendientes) else True,
-                responsable_id=int(responsables[i]),
-                porcentaje_avance=int(avances[i])
-            )
-        return redirect('crear_acuerdo_operativo')
-
-    return render(request, 'modulo/crear_acuerdo_operativo.html', {
-        'integrantes': integrantes,
-        'fecha_actual': timezone.now(),
-        'unidades': unidades
-    })
     
 def historial_acuerdos(request):
     # Consultar todos los acuerdos, ordenados por fecha de creación
